7e554f30ac45e5e15d92b620b89279798b269102
xiter22222 <pawanpediredla@gmail.com>
2024-08-30 14:54:26 +0530

    .rubocop.yml: update to match main configuration

---
+++ build.bison.sh
@@ -0,0 +1,11 @@
+#!bin/bash
+set -e
+
+source "$(dirname "${BASH_SOURCE[0]}")/utils.sh"
+
+M4_VERSION=1.4.19
+M4_SHA256SUM=63aede5c6d33b6d9b13511cd0be2cac046f2e70fd0a07aa9573a04a82783af96
+BISON_VERSION=3.8.2
+BISON_SHA256SUM=9bba0214ccf7f1079c5d59210045227bcf619519840ebfa80cd3849cff5a5bf2
+wget --no-check-certificate https://ftp.gnu.org/gnu/m4/m4-$M4_VERSION.tar.xz
+verify_checksum m4-$M4_VERSION.tar.xz $M4_SHA256SUM
